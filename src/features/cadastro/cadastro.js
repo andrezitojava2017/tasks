59d4167b1df81eb96f5bd96edfde603f73bf2d92
Jederson Andre <andre_sjx@live.com>
2022-09-27 15:56:33 -0300

    create page cadastro from users

--- src/features/cadastro/cadastro.js
+++ src/features/cadastro/cadastro.js
@@ -1,8 +1,50 @@
-const Cadastro = ()=>{
-    return (
-        <div>
-            <h1>Cadastre-se!!</h1>
-        </div>
-    );
-}
+import { Button, Container, FormControl, Grid, TextField } from "@mui/material";
+
+const Cadastro = () => {
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={6}>
+          <h1>Cadastre-se</h1>
+          <FormControl fullWidth>
+            <TextField
+              id="nameUser"
+              label="Digite seu nome"
+              variant="outlined"
+              sx={{ padding: "8px" }}
+            />
+
+            <TextField
+              id="email"
+              label="E-mail"
+              variant="outlined"
+              type="email"
+              sx={{ padding: "8px" }}
+            />
+
+            <TextField
+              id="password"
+              label="Senha"
+              variant="outlined"
+              type="password"
+              sx={{ padding: "8px" }}
+            />
+          </FormControl>
+          <Grid item>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              sx={{ marginBottom: "5px" }}
+            >
+              Cadastrar
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid item xs={3}></Grid>
+      </Grid>
+    </Container>
+  );
+};
 export default Cadastro;
