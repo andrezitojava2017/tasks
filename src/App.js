e83cb1920136aa2e95351639f1a013e2161cb3a3
Jederson Andre <andre_sjx@live.com>
2022-09-28 09:28:31 -0300

    move page home from folder features

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "./pages/Home";
+import Home from "./features/home/Home";
 import Login from "./features/login/login";
 import Cadastre from './features/cadastro/cadastro'
 import { Routes, Route } from "react-router-dom";
