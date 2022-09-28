3510ce88e933a0d397df6aaba05708fe9b6ee1a2
Jederson Andre <andre_sjx@live.com>
2022-09-28 11:56:19 -0300

    create estruture of page Home

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,12 +1,13 @@
-import { Avatar, Box, Container, Grid, Stack } from "@mui/material";
+import { Avatar, Container, Grid, Paper, Stack } from "@mui/material";
+import FormatListNumberedOutlinedIcon from "@mui/icons-material/FormatListNumberedOutlined";
 import Logo from "../../_assets/images/icons8-unpin-48.png";
 import Tasks from "../../components/tasks";
 import React from "react";
 const Home = () => {
   return (
     <Container>
-      <Grid container>
-        <Grid xs={12}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
           <Stack
             direction="row"
             justifyContent="flex-end"
@@ -16,8 +17,51 @@ const Home = () => {
             <Avatar alt="Remy Sharp" src={Logo} />
           </Stack>
         </Grid>
-        <Grid xs>
+        <Grid item xs>
           <Tasks />
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs>
+          <Container>
+            <Paper elevation={3} sx={{width:'150px', height:'120px', padding: "15px", textAlign: "center" }}>
+              <FormatListNumberedOutlinedIcon
+                sx={{ fontSize: 65, color: "#00C6CF", textAlign: "center" }}
+              />
+              <br />
+              <span>Adicionar Tarefa</span>
+            </Paper>
+          </Container>
+        </Grid>
+
+        <Grid item xs>
+          <Container>
+            <Paper elevation={3} sx={{width:'150px', height:'120px', padding: "15px", textAlign: "center" }}>
+              <FormatListNumberedOutlinedIcon
+                sx={{ fontSize: 65, color: "#00C6CF", textAlign: "center" }}
+              />
+              <br />
+              <span>Minhas Tarefas</span>
+            </Paper>
+          </Container>
+        </Grid>
+
+        <Grid item xs>
+          <Container>
+            <Paper elevation={3} sx={{ width:'150px', height:'120px', padding: "15px", textAlign: "center" }}>
+              <FormatListNumberedOutlinedIcon
+                sx={{ fontSize: 65, color: "#00C6CF", textAlign: "center" }}
+              />
+              <br />
+              <span>Relatorios</span>
+            </Paper>
+          </Container>
         </Grid>
       </Grid>
     </Container>
