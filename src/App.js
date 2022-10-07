d7c4fa3e5ff21d72c014251c89bdddd7bd8547c8
Jederson Andre <andre_sjx@live.com>
2022-10-07 16:18:02 -0300

    add new route from novaTarefa

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Perfil from './features/perfil/perfil';
 import Home from './features/home/Home';
 import Login from './features/login/login';
 import Cadastre from './features/cadastro/cadastro';
-import Tarefas from './features/listaTarefas/tarefas';
+import Tarefas from './features/tarefas/listTarefas';
 import { Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
-import { Container, CssBaseline } from '@mui/material';
+import { CssBaseline } from '@mui/material';
+import NovaTarefa from './features/tarefas/novaTarefa';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/cadastro" element={<Cadastre />} />
           <Route path="/perfil" element={<Perfil />} />
           <Route path="/tarefas" element={<Tarefas />} />
+          <Route path="/tarefas/nova" element={<NovaTarefa />} />
         </Routes>
       </Box>
       </>
