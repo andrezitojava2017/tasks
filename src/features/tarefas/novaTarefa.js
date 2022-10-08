46203fe64593ae68d045ab71f592e41c328df241
Jederson Andre <andre_sjx@live.com>
2022-10-08 16:30:36 -0300

    modified new task form

--- src/features/tarefas/novaTarefa.js
+++ src/features/tarefas/novaTarefa.js
@@ -1,56 +1,88 @@
-import { FormControl, Grid, TextField } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
+import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
+import React from 'react';
 
 const NovaTarefa = () => {
   return (
     <Grid
       container
       spacing={2}
-      sx={{ marginTop: "15px", marginBottom: "15px" }}
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ marginTop: '15px', marginBottom: '15px', border: '1px solid red' }}
     >
-      <Grid xs={3}></Grid>
+      <Grid item>
+        <Typography variant="h4" sx={{ textAlign: 'center' }}>
+          <span>Nova Tarefa</span>
+        </Typography>
+      </Grid>
       <Grid item xs={6}>
-        <Grid item xs={6}>
-          
+        <Container>
+          <Grid item>
             <TextField
-            fullWidth
+              fullWidth
               id="titulo"
               label="Titulo"
               variant="outlined"
-              sx={{ padding: "8px" }}
-              size="small"
+              type="text"
+              sx={{ padding: '8px' }}
             />
-         
-        </Grid>
-        <Grid item xs={6}>
+          </Grid>
+        </Container>
+        <Container>
           <TextField
             id="data"
             hiddenLabel
             variant="outlined"
             type="date"
-            sx={{ padding: "8px" }}
-            size="small"
+            sx={{ padding: '8px' }}
           />
           <TextField
             hiddenLabel
             id="horaInicio"
             variant="outlined"
             type="time"
-            sx={{ padding: "8px" }}
-            size="small"
+            sx={{ padding: '8px' }}
           />
           <TextField
             id="horaFim"
             hiddenLabel
             variant="outlined"
             type="time"
-            sx={{ padding: "8px" }}
-            size="small"
+            sx={{ padding: '8px' }}
           />
-        </Grid>
+        </Container>
+        <Container>
+          <Grid item>
+            <TextField
+              fullWidth
+              multiline
+              id="descricao"
+              label="Descrição"
+              variant="outlined"
+              type="text"
+              sx={{ padding: '8px' }}
+            />
+          </Grid>
+        </Container>
+        <Container>
+          <Button variant="contained" sx={{ marginLeft: '10px' }}>
+            <span>SALVAR</span>
+          </Button>
+          <Button variant="contained" sx={{ marginLeft: '10px' }}>
+            <span>CANCELAR</span>
+          </Button>
+        </Container>
       </Grid>
-
-      <Grid xs={3}></Grid>
     </Grid>
   );
 };
