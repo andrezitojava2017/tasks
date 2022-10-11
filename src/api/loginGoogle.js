b60f1229e797d476e249fa66061d52b07da9335e
Jederson Andre <andre_sjx@live.com>
2022-10-10 21:42:28 -0300

    altered logic of login

--- src/api/loginGoogle.js
+++ src/api/loginGoogle.js
@@ -1,8 +1,8 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import ConfigFireBase from "./configFireBase"
-
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import ConfigFireBase from './configFireBase';
+import AlertMessage from '../components/alertMessage';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 // Initialize Firebase
 const app = initializeApp(ConfigFireBase);
@@ -10,25 +10,21 @@ const analytics = getAnalytics(app);
 
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
-const LoginGoogle = ()=>{
-    signInWithPopup(auth, provider)
+const LoginGoogle = async () => {
+  const dat = await signInWithPopup(auth, provider)
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
       // The signed-in user info.
-      const user = result.user;
-      console.log(user.displayName)
-      // ...
-    }).catch((error) => {
+
+      return result.user;
+    })
+    .catch((error) => {
       // Handle Errors here.
-      const errorCode = error.code;
       const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
+      return [{ view: true, message: errorMessage }];
     });
-}
+  return dat;
+};
 export default LoginGoogle;
