247930797bfd2d674fc286e2675679e74c7cd020
Jederson Andre <andre_sjx@live.com>
2022-10-13 22:14:57 -0300

    update layout of form

--- src/features/login/login.js
+++ src/features/login/login.js
@@ -1,72 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Styles from './login.module.css';
-import { Box, Collapse } from '@mui/material';
+import { Container, Divider, FormControl, Stack } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Logo from '../../_assets/images/ico_tasks_human.png';
 import Tasks from '../../components/tasks';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
-import LoginInGoogle from '../../components/logInGoogle';
+import LoginInGoogle from '../../features/login/logInGoogle';
 
 const Login = () => {
   return (
-    <Box>
-      <Grid container spacing={2} direction="column" alignItems="center">
-        <Grid item xs={6}>
-          <img alt="Logo tasks" src={Logo} className={Styles.img} />
-        </Grid>
-
-        <Grid item xs={6}>
-          <Tasks />
-        </Grid>
-      </Grid>
-      <Grid container spacing={2} direction="column" alignItems="center">
-        <Grid item xs={12}>
-          <TextField
-            id="outlined-basic"
-            fullWidth
-            label="Usuario"
-            variant="outlined"
-            size="medium"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            id="password"
-            label="Senha"
-            variant="outlined"
-            type="password"
-            size="medium"
-          />
-        </Grid>
-      </Grid>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item xs={12} mt={2}>
-          <Link to="/cadastro">Cadastre-se</Link>
-        </Grid>
-        <Grid item xs={6} mt={2}>
-          <LoginInGoogle />
-        </Grid>
-      </Grid>
+    <Container>
       <Grid
         container
         spacing={2}
         direction="column"
-        justifyContent="center"
         alignItems="center"
+        justifyItems="center"
+        sx={{ marginTop: '15px', marginBottom: '15px' }}
       >
-        <Grid item xs={12} mt={2}>
-          <Button variant="contained">Logar</Button>
+        <Stack>
+          <img alt="Logo tasks" src={Logo} className={Styles.img} />
+        </Stack>
+
+        <Stack>
+          <Tasks />
+        </Stack>
+
+        <Grid item>
+          <FormControl fullWidth>
+            <TextField
+              id="outlined-basic"
+              label="Usuario"
+              variant="outlined"
+              size="medium"
+            />
+
+            <TextField
+              id="password"
+              label="Senha"
+              variant="outlined"
+              type="password"
+              size="medium"
+            />
+          </FormControl>
+
+          <Grid container justifyContent="space-between">
+            <Stack sx={{ paddingTop: '10px' }}>
+              <Link to="/cadastro">Cadastre-se</Link>
+            </Stack>
+            <Stack sx={{ paddingTop: '10px' }}>
+              <Button variant="contained">Logar</Button>
+            </Stack>
+          </Grid>
+
+          <Divider sx={{ marginTop: '15px' }} />
+          <Grid item mt={2}>
+            <LoginInGoogle />
+          </Grid>
         </Grid>
       </Grid>
-    </Box>
+    </Container>
   );
 };
 
