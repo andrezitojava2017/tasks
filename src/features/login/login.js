ab425359d8b6f0cdead858f2b37bc1174a0a625b
Jederson Andre <andre_sjx@live.com>
2022-10-14 09:39:02 -0300

    accept updates of github

--- src/features/login/login.js
+++ src/features/login/login.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import Styles from './login.module.css';
-<<<<<<< HEAD
-import { Box, Collapse, Container, Divider } from '@mui/material';
-=======
 import { Container, Divider, FormControl, Stack } from '@mui/material';
->>>>>>> 97c1716530bad3c62b7e15433ea08bf038204863
 import Grid from '@mui/material/Grid';
 import Logo from '../../_assets/images/ico_tasks_human.png';
 import Tasks from '../../components/tasks';
@@ -21,60 +17,6 @@ const Login = () => {
 
   return (
     <Container>
-<<<<<<< HEAD
-      <Grid container spacing={2} direction="column" alignItems="center">
-        <Grid item xs={6}>
-          <img alt="Logo tasks" src={Logo} className={Styles.img} />
-        </Grid>
-
-        <Grid item xs={6}>
-          <Tasks />
-        </Grid>
-      </Grid>
-      <Grid container spacing={2} direction="column" alignItems="center">
-        <Grid item xs={12}>
-          <TextField
-            id="outlined-basic"
-            fullWidth
-            label="Usuario"
-            variant="outlined"
-            size="medium"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            id="password"
-            label="Senha"
-            variant="outlined"
-            type="password"
-            size="medium"
-          />
-        </Grid>
-      </Grid>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item xs={6} mt={2}>
-          <Link to="/cadastro">Cadastre-se</Link>
-        </Grid>
-        <Grid item xs={12} mt={2}>
-          <Button variant="contained" onClick={handleGoogle}>
-            Google
-          </Button>
-        </Grid>
-      </Grid>
-      <Grid container spacing={2} justifyContent="center" alignItems="center">
-        <Grid item xs={6} mt={2}>
-          <Divider />
-        </Grid>
-      </Grid>
-
-=======
->>>>>>> 97c1716530bad3c62b7e15433ea08bf038204863
       <Grid
         container
         spacing={2}
@@ -83,12 +25,6 @@ const Login = () => {
         justifyItems="center"
         sx={{ marginTop: '15px', marginBottom: '15px' }}
       >
-<<<<<<< HEAD
-        <Grid item xs={12} mt={2}>
-          <Button variant="contained" color="error" onClick={handleGoogle}>
-            entrar com Google
-          </Button>
-=======
         <Stack>
           <img alt="Logo tasks" src={Logo} className={Styles.img} />
         </Stack>
@@ -130,7 +66,6 @@ const Login = () => {
           <Grid item mt={2}>
             <LoginInGoogle />
           </Grid>
->>>>>>> 97c1716530bad3c62b7e15433ea08bf038204863
         </Grid>
       </Grid>
     </Container>
