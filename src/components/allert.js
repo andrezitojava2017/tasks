92b4554c0db38f1e946d044c45d3c71bb9c1327c
Jederson Andre <andre_sjx@live.com>
2022-10-16 09:41:28 -0300

    update component allert from return error or success

--- src/components/allert.js
+++ src/components/allert.js
@@ -4,31 +4,58 @@ import Stack from '@mui/material/Stack';
 import { Collapse, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
-const Allert = ({ open, setOpen, errorMessage }) => {
-  return (
-    <Stack>
-      <Collapse in={open}>
-        <Alert
-          severity="error"
-          action={
-            <IconButton
-              aria-label="close"
-              color="inherit"
-              size="small"
-              onClick={() => {
-                setOpen(false);
-              }}
-            >
-              <CloseIcon fontSize="inherit" />
-            </IconButton>
-          }
-          sx={{ mb: 2 }}
-        >
-          {`Ocorreu um erro: ${errorMessage}`}
-        </Alert>
-      </Collapse>
-    </Stack>
-  );
+const Allert = ({ type, open, setOpen, message }) => {
+  if (type == 'error') {
+    return (
+      <Stack>
+        <Collapse in={open}>
+          <Alert
+            severity="error"
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+            }
+            sx={{ mb: 2 }}
+          >
+            {`Ocorreu um erro: ${message}`}
+          </Alert>
+        </Collapse>
+      </Stack>
+    );
+  } else {
+    return (
+      <Stack>
+        <Collapse in={open}>
+          <Alert
+            severity="success"
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+            }
+            sx={{ mb: 2 }}
+          >
+            {`${message}`}
+          </Alert>
+        </Collapse>
+      </Stack>
+    );
+  }
 };
 
 export default Allert;
