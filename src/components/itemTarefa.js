b4bcb0436053de0174d9484dd1c4a391357160f1
Jederson Andre <andre_sjx@live.com>
2022-10-18 17:57:55 -0300

    update component itemTarefa

--- src/components/itemTarefa.js
+++ src/components/itemTarefa.js
@@ -1,23 +1,20 @@
 import { Grid } from '@mui/material';
 
 const ItemTarefa = (props) => {
-  const test = (event) => {
-    console.log(event.target.value);
-  };
 
   return (
     <>
-      <Grid item>
+      <Grid item >
         <div>
           <ul style={{ listStyle: 'none', padding: '12px' }}>
             <li>
               <label>
                 <input
                   type="checkbox"
-                  name="titulo"
-                  id="item1"
-                  value="Item 1"
-                  onChange={test}
+                  id={props.indice}
+                  value={props.tasks.title}
+                  onChange={props.change}
+                  
                 />
                 {`${props.tasks.title}`}
               </label>
