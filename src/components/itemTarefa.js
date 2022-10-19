5d1d4b936ef75c571c8131141c01d569430fa3b0
Jederson Andre <andre_sjx@live.com>
2022-10-19 11:23:31 -0300

    created task status field

--- src/components/itemTarefa.js
+++ src/components/itemTarefa.js
@@ -1,18 +1,19 @@
-import { Grid } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Grid } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 const ItemTarefa = (props) => {
   return (
     <>
       <Grid item>
         <div>
-          <ul style={{ listStyle: 'none', padding: '12px' }}>
+          <ul style={{ listStyle: "none", padding: "12px" }}>
             <li
               style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-              }}>
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
               <label>
                 <input
                   type="checkbox"
@@ -27,10 +28,17 @@ const ItemTarefa = (props) => {
                 <IconButton
                   edge="end"
                   aria-label="comments"
-                  onClick={props.removeItemChecked}>
+                  onClick={props.removeItemChecked}
+                >
                   <DeleteIcon />
                 </IconButton>
               </div>
+            </li>
+            <li style={{ fontSize: "10px", marginLeft: "15px" }}>
+              Situação desta tarefa:{" "}
+              <span style={{color:'red'}}>
+                {props.tasks.situation === true ? "Concluida" : "Não concluida"}
+              </span>
             </li>
           </ul>
         </div>
