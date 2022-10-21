23ce2ce865e05545a0a906f24be65d7104cce54c
Jederson Andre <andre_sjx@live.com>
2022-10-21 16:53:20 -0300

    implementação do menu suspenso, disponivel na foto do usuario

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,58 @@
-import { Avatar, Grid, Stack } from '@mui/material';
-import { Link } from 'react-router-dom';
-import Logo from '../_assets/images/icons8-unpin-48.png';
+import { Avatar, Grid, IconButton, Menu, MenuItem, Stack } from "@mui/material";
+import { useState } from "react";
+import {signnOut} from '../api/loginFunctions'
 
 const Header = () => {
-  let photo = JSON.parse(sessionStorage.getItem('data'))
-  
+  let photo = JSON.parse(sessionStorage.getItem("data"));
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    (async ()=>{
+      await signnOut()
+      sessionStorage.removeItem('data');
+      window.location.assign('/')
+    })();
+    setAnchorEl(null);
+  };
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} sm={8} sx={{ marginTop: '15px' }}>
+      <Grid item xs={12} sm={8} sx={{ marginTop: "15px" }}>
         <Stack
           direction="row"
           justifyContent="flex-end"
           alignItems="flex-start"
         >
+          <IconButton
+            aria-label="delete"
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <Avatar alt="Foto usuario" src={photo.photoURL} /> 
+          </IconButton>
+          
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleClose}>Sair</MenuItem>
+          </Menu>
+
+          {/*
           <Link to="/perfil">
-            <Avatar alt="Foto usuario" src={photo.photoURL} />
+            
           </Link>
+          */}
         </Stack>
       </Grid>
     </Grid>
