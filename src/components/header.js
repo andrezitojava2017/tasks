acaacd7846eae0e3e5a7c43b2238317a28cd9485
Jederson Andre <andre_sjx@live.com>
2022-10-25 11:32:17 -0300

    remoção de funçoes nao utilizadas e alteração no tamanho do avatar

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,10 @@
 import { Avatar, Grid, IconButton, Menu, MenuItem, Stack } from "@mui/material";
 import { useState } from "react";
-import {signnOut} from '../api/loginFunctions'
+import { signnOut } from "../api/loginFunctions";
 
 const Header = () => {
   let photo = JSON.parse(sessionStorage.getItem("data"));
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    (async ()=>{
-      await signnOut()
-      sessionStorage.removeItem('data');
-      window.location.assign('/')
-    })();
-    setAnchorEl(null);
-  };
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} sm={8} sx={{ marginTop: "15px" }}>
@@ -25,11 +13,12 @@ const Header = () => {
           justifyContent="flex-end"
           alignItems="flex-start"
         >
-          <IconButton
-
-            onClick={handleClick}
-          >
-            <Avatar alt="Foto usuario" src={photo.photoURL} /> 
+          <IconButton>
+            <Avatar
+              alt="Foto usuario"
+              src={photo.photoURL}
+              sx={{ width: 56, height: 56 }}
+            />
           </IconButton>
         </Stack>
       </Grid>
