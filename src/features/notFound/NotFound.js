c0010df8128ddab97c8d81611b5a93e71fed2915
Jederson Andre <andre_sjx@live.com>
2022-11-04 20:59:16 -0300

    adicionado rota de retorno para pagina principal

--- src/features/notFound/NotFound.js
+++ src/features/notFound/NotFound.js
@@ -1,11 +1,18 @@
+import { Box, Stack } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NotFound = ()=>{
-return (
+const NotFound = () => {
+  return (
     <>
-    <h2>Erro 404 - pagina nao encontrada</h2>
+      <h2>Erro 404 - pagina nao encontrada</h2>
+      <Stack direction={'row'} justifyContent="center">
+        <Box style={{ marginTop: '20px' }}>
+          <Link to="/">Principal</Link>
+        </Box>
+      </Stack>
     </>
-)
-}
+  );
+};
 
 export default NotFound;
