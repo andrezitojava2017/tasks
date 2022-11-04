a773dec060d8c98cfde333981083cef27b46b22f
Jederson Andre <andre_sjx@live.com>
2022-11-04 20:59:59 -0300

    atualização na cor do background e foi adicionado evento de click no avatar

--- src/features/perfil/perfil.js
+++ src/features/perfil/perfil.js
@@ -1,44 +1,47 @@
-import { Avatar, Container, Grid, Stack, Typography } from '@mui/material';
+import { Avatar, Box, Container, Grid, Stack, Typography } from '@mui/material';
 import GoogleIcon from '@mui/icons-material/Google';
-import Image from '../../_assets/images/icons8-unpin-48.png';
+import { Link } from 'react-router-dom';
 
-const Perfil = (props) => {
+const Perfil = () => {
+  let user = JSON.parse(sessionStorage.getItem('data'));
+
   return (
     <Container>
       <Grid
         container
         justifyContent="center"
         alignItems="center"
-        sx={{ border: '1px solid red' }}
-      >
+        sx={{ backgroundColor: '#7FD1AE', marginTop: '20%' }}>
         <Avatar
           alt="Foto perfil"
-          src={Image}
+          src={user.photoURL}
           sx={{ width: '8rem', height: '8rem' }}
         />
-      </Grid>
-      <Grid item xs={12}>
-        <Typography
-          variant="h5"
-          component="h3"
-          sx={{ textAlign: 'center', marginTop: '-10px' }}
-        >
-          <h3>{props.nome}</h3>
-        </Typography>
-      </Grid>
-
-      <Grid item xs={12}>
+        <Grid item xs={12}>
+          <Typography
+            variant="h5"
+            component="h3"
+            sx={{ textAlign: 'center', marginTop: '-10px' }}>
+            <h3>{user.displayName ? user.displayName : user.name}</h3>
+          </Typography>
+        </Grid>
         <Stack
           direction="row"
           spacing={2}
           justifyContent="center"
-          alignItems="center"
-        >
+          alignItems="center">
           <Avatar alt="E-mail usuario">
             <GoogleIcon />
           </Avatar>
-          <Typography variant="body1">{props.email}</Typography>
+          <Typography variant="p">{user.email}</Typography>
         </Stack>
+        <Grid item xs={12}>
+          <Stack direction={'row'} justifyContent="center">
+            <Box style={{ marginTop: '20px' }}>
+              <Link to="/">Principal</Link>
+            </Box>
+          </Stack>
+        </Grid>
       </Grid>
     </Container>
   );
