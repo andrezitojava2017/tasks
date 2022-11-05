dc39057f35be00a2139be26fcb56f3df1571bf74
Jederson Andre <andre_sjx@live.com>
2022-11-04 21:00:45 -0300

    retirada de imports nao utilizados

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,27 @@
-import { Avatar, Grid, IconButton, Menu, MenuItem, Stack } from "@mui/material";
-import { useState } from "react";
-import { signnOut } from "../api/loginFunctions";
+import { Avatar, Grid, IconButton, Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const photo = JSON.parse(sessionStorage.getItem("data"));
+  const photo = JSON.parse(sessionStorage.getItem('data'));
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={8} sx={{ marginTop: "15px" }}>
+    <Grid
+      container
+      spacing={2}
+      sx={{ backgroundColor: '#7FD1AE', marginBottom: '23px' }}>
+      <Grid item xs={12} sm={10} sx={{ marginTop: '12px' }}>
         <Stack
           direction="row"
           justifyContent="flex-end"
-          alignItems="flex-start"
-        >
+          alignItems="flex-start">
           <IconButton>
-            <Avatar
-              alt="Foto usuario"
-              src={photo.photoURL}
-              sx={{ width: 56, height: 56 }}
-            />
+            <Link to="/perfil">
+              <Avatar
+                alt="Foto usuario"
+                src={photo.photoURL}
+                sx={{ width: 56, height: 56 }}
+              />
+            </Link>
           </IconButton>
         </Stack>
       </Grid>
